3385cebddf920fdf50f22bdf61eb93cfc11d8405
mahmoodakhtar60 <makhtar1@student.ohlone.edu>
2024-10-12 14:01:11 -0700

    Add files via upload

---
+++ music.asm
@@ -0,0 +1,310 @@
+.text
+
+li $a2 124 #load synth effect 124 96
+li $a3 90 #set the volume
+
+li $a0 60 # play middle C
+li $a1 1000 #play for one second
+
+li $v0 31 #play immediately
+syscall
+
+li $v0 32
+li $a0 200
+syscall
+
+
+li $a2 124 #load synth effect
+li $a3 90 #set the volume
+
+li $a0 60 # play middle C
+li $a1 1000 #play for one second
+
+li $v0 31 #wait until play
+syscall
+
+li $v0 32
+li $a0 300
+syscall
+
+
+li $a2 124 #load synth effect 124 96
+li $a3 90 #set the volume
+
+li $a0 60 # play middle C
+li $a1 800 #play for one second
+
+li $v0 31 #wait
+syscall
+
+li $v0 32
+li $a0 100
+syscall
+
+
+li $a2 124 #load synth effect
+li $a3 90 #set the volume
+
+li $a0 60 # play middle C
+li $a1 1000 #play for one second
+
+li $v0 31 #play immediately
+syscall
+
+li $v0 32
+li $a0 100
+syscall
+
+li $a2 124 #load synth effect 124 96
+li $a3 90 #set the volume
+
+li $a0 60 # play middle C
+li $a1 1000 #play for one second
+
+li $v0 31 #play immediately
+syscall
+
+
+li $v0 32
+li $a0 100
+syscall
+
+li $a2 124 #load synth effect
+li $a3 90 #set the volume
+
+li $a0 60 # play middle C
+li $a1 1000 #play for one second
+
+li $v0 31 #play immediately
+syscall
+
+li $v0 32
+li $a0 300
+syscall
+
+li $a2 124 #load synth effect 124 96
+li $a3 90 #set the volume
+
+li $a0 60 # play middle C
+li $a1 1000 #play for one second
+
+li $v0 31 #play immediately
+syscall
+
+li $v0 32
+li $a0 100
+syscall
+
+li $a2 124 #load synth effect
+li $a3 90 #set the volume
+
+li $a0 60 # play middle C
+li $a1 800 #play for one second
+
+li $v0 31 #play immediately
+syscall
+
+li $v0 32
+li $a0 100
+syscall
+
+li $a2 96 #load synth effect
+li $a3 90 #set the volume
+
+li $a0 67 # play middle C
+li $a1 550 #play for one second
+
+li $v0 33
+syscall
+
+li $a2 96 #load synth effect
+li $a3 90 #set the volume
+
+li $a0 69 # play middle C
+li $a1 700 #play for one second
+
+li $v0 33
+syscall
+
+li $a2 96 #load synth effect
+li $a3 90 #set the volume
+
+li $a0 69 # play middle C
+li $a1 500 #play for one second
+
+li $v0 33
+syscall
+
+li $a2 96 #load synth effect
+li $a3 90 #set the volume
+
+li $a0 71 # play middle C
+li $a1 600 #play for one second
+
+li $v0 33
+syscall
+
+li $a2 96 #load synth effect
+li $a3 90 #set the volume
+
+li $a0 72 # play middle C
+li $a1 300 #play for one second
+
+li $v0 33
+syscall
+li $a2 124 #load synth effect 124 96
+li $a3 90 #set the volume
+
+li $a0 60 # play middle C
+li $a1 1000 #play for one second
+
+li $v0 31 #play immediately
+syscall
+
+li $v0 32
+li $a0 200
+syscall
+
+
+li $a2 124 #load synth effect
+li $a3 90 #set the volume
+
+li $a0 60 # play middle C
+li $a1 1000 #play for one second
+
+li $v0 31 #wait until play
+syscall
+
+li $v0 32
+li $a0 300
+syscall
+
+
+li $a2 124 #load synth effect 124 96
+li $a3 90 #set the volume
+
+li $a0 60 # play middle C
+li $a1 800 #play for one second
+
+li $v0 31 #wait
+syscall
+
+li $v0 32
+li $a0 100
+syscall
+
+
+li $a2 124 #load synth effect
+li $a3 90 #set the volume
+
+li $a0 60 # play middle C
+li $a1 1000 #play for one second
+
+li $v0 31 #play immediately
+syscall
+
+li $v0 32
+li $a0 100
+syscall
+
+li $a2 124 #load synth effect 124 96
+li $a3 90 #set the volume
+
+li $a0 60 # play middle C
+li $a1 1000 #play for one second
+
+li $v0 31 #play immediately
+syscall
+
+
+li $v0 32
+li $a0 100
+syscall
+
+li $a2 124 #load synth effect
+li $a3 90 #set the volume
+
+li $a0 60 # play middle C
+li $a1 1000 #play for one second
+
+li $v0 31 #play immediately
+syscall
+
+li $v0 32
+li $a0 300
+syscall
+
+li $a2 124 #load synth effect 124 96
+li $a3 90 #set the volume
+
+li $a0 60 # play middle C
+li $a1 1000 #play for one second
+
+li $v0 31 #play immediately
+syscall
+
+li $v0 32
+li $a0 100
+syscall
+
+li $a2 124 #load synth effect
+li $a3 90 #set the volume
+
+li $a0 60 # play middle C
+li $a1 800 #play for one second
+
+li $v0 31 #play immediately
+syscall
+
+li $v0 32
+li $a0 100
+syscall
+
+li $a2 96 #load synth effect
+li $a3 90 #set the volume
+
+li $a0 67 # play middle C
+li $a1 550 #play for one second
+
+li $v0 33
+syscall
+
+li $a2 96 #load synth effect
+li $a3 90 #set the volume
+
+li $a0 69 # play middle C
+li $a1 700 #play for one second
+
+li $v0 33
+syscall
+
+li $a2 96 #load synth effect
+li $a3 90 #set the volume
+
+li $a0 69 # play middle C
+li $a1 500 #play for one second
+
+li $v0 33
+syscall
+
+li $a2 96 #load synth effect
+li $a3 90 #set the volume
+
+li $a0 71 # play middle C
+li $a1 600 #play for one second
+
+li $v0 33
+syscall
+
+li $a2 96 #load synth effect
+li $a3 90 #set the volume
+
+li $a0 72 # play middle C
+li $a1 300 #play for one second
+
+li $v0 33
+syscall
+
+li $v0 10
+syscall
+
